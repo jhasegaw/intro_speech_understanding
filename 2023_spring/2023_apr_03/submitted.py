dd532b947b548148c6d0e2b26b11d11da3740a8e
Mark Hasegawa-Johnson <jhasegaw@illinois.edu>
2023-04-02 11:15:11 -0500

    foo

--- 2023_spring/2023_apr_03/submitted.py
+++ 2023_spring/2023_apr_03/submitted.py
@@ -1,3 +1,14 @@
+import numpy as np
 
-This is a starter file.
+def generate_pure_tone(frequency, sample_rate, duration):
+    '''
+    Generate a pure tone at the specified frequency, sampling rate, and duration.
+    @parameter:
+    frequency (scalar) - the frequency of the pure tone, in Hertz
+    sample_rate (scalar) - how many samples per second?
+    duration (scalar) - the duration of the pure tone, in seconds
+    '''
+    t = np.linspace(0,duration,sample_rate)
+    x = np.sine(2*np.pi*frequency*t)
+    return(x)
 
