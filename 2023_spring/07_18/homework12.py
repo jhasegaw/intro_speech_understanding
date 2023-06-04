b4f9a24bd412a3b8ad253a086f8ff45fbf74a9fe
Mark Hasegawa-Johnson <jhasegaw@illinois.edu>
2023-06-04 14:38:58 -0500

    07_18

--- 2023_spring/07_18/homework12.py
+++ 2023_spring/07_18/homework12.py
@@ -1,16 +1,29 @@
-import numpy as np
+import bs4, gtts
 
-def dft_matrix(N):
+def extract_stories_from_NPR_text(text):
     '''
-    Create a DFT transform matrix, W, of size N.
+    Extract a list of stories from the text of the npr.com webpage.
     
-    @param:
-    N (scalar): number of columns in the transform matrix
+    @params: 
+    text (string): the text of a webpage
     
-    @result:
-    W (NxN array): a matrix of dtype='complex' whose (k,n)^th element is:
-           W[k,n] = cos(2*np.pi*k*n/N) - j*sin(2*np.pi*k*n/N)
+    @returns:
+    stories (list of tuples of strings): a list of the news stories in the web page.
+      Each story should be a tuple of (title, teaser), where the title and teaser are
+      both strings.  If the story has no teaser, its teaser should be an empty string.
     '''
-    W = np.zeros((N,N),dtype='complex')
-    return W
+    raise RuntimeError('You need to write this part!')
+    return stories
+    
+def read_nth_story(stories, n, filename):
+    '''
+    Read the n'th story from a list of stories.
+    
+    @params:
+    stories (list of tuples of strings): a list of the news stories from a web page
+    n (int): the index of the story you want me to read
+    filename (str): filename in which to store the synthesized audio
 
+    Output: None
+    '''
+    raise RuntimeError('You need to write this part!')
