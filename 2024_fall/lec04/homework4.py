c4be9c3de4e1b34fdf3ac9b48f6b5ca6a3fa734a
M23W7090 <yangliuqing167167@qq.com>
2025-01-21 13:40:31 +0900

    hw4
    hw4

--- 2024_fall/lec04/homework4.py
+++ 2024_fall/lec04/homework4.py
@@ -5,8 +5,7 @@ def list_to_dict(input_list):
     `input_list` is a value, and the corresponding key is the numerical 
     index of that element in `input_list`. 
     '''
-    pass
-
-
-
-
+   newdict= {}
+    for n, v in enumerate(input_list):
+        newdict[n] = v
+    return newdict
