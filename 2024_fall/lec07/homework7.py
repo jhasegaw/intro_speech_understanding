4ac19bed53b7d3054e0fb19b1d5e2bc5072b64e5
M23W7090 <yangliuqing167167@qq.com>
2025-01-21 13:54:13 +0900

    hw7
    hw7

--- 2024_fall/lec07/homework7.py
+++ 2024_fall/lec07/homework7.py
@@ -15,7 +15,8 @@ def voiced_excitation(duration, F0, Fs):
       excitation[n] = 0 otherwise
     '''
     excitation = np.zeros(duration) 
-    pass # change this
+    T0 = int (np.round（Fs/F0))
+    excitation[::T0] = -1
     return excitation
 
 def resonator(x, F, BW, Fs):
@@ -32,10 +33,16 @@ def resonator(x, F, BW, Fs):
     y (np.ndarray(N)) - resonant output
     '''
     y = np.zeros(len(x)) 
-    pass # change this
+    C = -np.exp(-2*np.pi*BW/Fs)
+    B = 2 * np.exp(-np.pi*BW/Fs) * np.cos(2*np.pi*F/Fs)
+    A = 1 - B - C
+    y = np.zeros(len(x))
+    y[0] = A*x[0]
+    y[1] = A*x[1] + B*y[0]
+    for n in range(2,len(y)):
+        y[n] = A*x[n] + B*y[n-1] + C*y[n-2]
     return y
-
-def synthesize_vowel(duration,F0,F1,F2,F3,F4,BW1,BW2,BW3,BW4,Fs):
+    def synthesize_vowel(duration,F0,F1,F2,F3,F4,BW1,BW2,BW3,BW4,Fs):
     '''
     Synthesize a vowel.
     
@@ -55,6 +62,10 @@ def synthesize_vowel(duration,F0,F1,F2,F3,F4,BW1,BW2,BW3,BW4,Fs):
     @returns:
     speech (np.ndarray(samples)) - synthesized vowel
     '''
-    speech = np.zeros(duration) # change this
+    escitation = voiced_excitation(duration, F0, Fs)
+    y1 = resonator(excitation,F1,BW1,Fs)
+    y2 = resonator(y1,F2,BW2,Fs)
+    y3 = resonator(y2,F3,BW3,Fs)
+    speech = resonator(y3,F4,BW4,Fs)
     return speech
     
