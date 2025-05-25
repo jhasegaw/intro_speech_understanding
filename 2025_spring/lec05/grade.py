3b27768d476e65d1fa06ee6ea47e79e8f906fee9
atsushi-morisue <st162338@m2.kcg.edu>
2025-05-25 21:53:48 +0900

    lec03_notebook, grade.pyの更新とUntitled.ipynbの追加

--- 2025_spring/lec05/grade.py
+++ 2025_spring/lec05/grade.py
@@ -30,6 +30,8 @@ class Test(unittest.TestCase):
 suite = unittest.defaultTestLoader.loadTestsFromTestCase(Test)
 result = unittest.TextTestRunner().run(suite)
 
+
+
 n_success = result.testsRun - len(result.errors) - len(result.failures)
 print('%d successes out of %d tests run'%(n_success, result.testsRun))
 print('Score: %d%%'%(int(100*(n_success/result.testsRun))))
