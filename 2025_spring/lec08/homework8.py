ef5f5dad60387e815a050d229eb32d18755d0e1d
atsushi-morisue <st162338@m2.kcg.edu>
2025-06-12 11:06:18 +0900

    I have done homework8

--- 2025_spring/lec08/homework8.py
+++ 2025_spring/lec08/homework8.py
@@ -11,5 +11,12 @@ def dft_matrix(N):
     W (NxN array): a matrix of dtype='complex' whose (k,n)^th element is:
            W[k,n] = cos(2*np.pi*k*n/N) - j*sin(2*np.pi*k*n/N)
     '''
-    raise RuntimeError("You need to write this part")
+    #raise RuntimeError("You need to write this part")
+    W = np.zeros((N,N),dtype='complex')
+    for k in range(0,N):
+        for n in range(0,N):
+            W[k,n] = np.cos(2*np.pi*k*n/N)-(0+1j)*np.sin(2*np.pi*k*n/N)
+    return W
+
+
 
