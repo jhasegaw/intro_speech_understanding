2dc7ac6eda7a23eb4d51581f0b3795b33ed1655c
atsushimorisue2022-pixel <atsushi.morisue.2022@globis.ac.jp>
2025-07-25 23:36:54 +0900

    homework11 was done

--- 2025_spring/lec11/homework11.py
+++ 2025_spring/lec11/homework11.py
@@ -13,4 +13,9 @@ def transcribe_wavefile(filename, language='en'):
     @returns:
     text (str) - the recognized speech
     '''
-    raise RuntimeError("FAIL!!  You need to change this function so it works!")
+    #raise RuntimeError("FAIL!!  You need to change this function so it works!")
+    r = speech_recognition.Recognizer()
+    with speech_recognition.AudioFile(filename) as source:
+        audio = r.record(source)
+        text = r.recognize_google(audio, language=language)
+    return text
